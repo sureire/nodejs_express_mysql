d2e1c46ddb97f0d384e935d35c94f3520e3be741
sureire <38441738+sureire@users.noreply.github.com>
2021-02-09 21:11:18 -0800

    added app.yaml

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,28 @@
+// const mysql = require("mysql");
+// const dbConfig = require("../config/db.config.js");
+
+// var connection = mysql.createPool({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB
+// });
+
+// module.exports = connection;
+
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
+//const dbSocketAddr = process.env.DB_HOST.split(':');
+ const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+
+
 var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  user: process.env.DB_USER, //dbConfig.USER,
+  password: process.env.DB_PASS, //dbConfig.PASSWORD,
+  database: process.env.DB_NAME, //dbConfig.DB
+    socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`
+  //port: dbSocketAddr[1]
 });
 
 module.exports = connection;
