d79ddcb8ed784da8333bede13bcd1319432b1d25
sureire <38441738+sureire@users.noreply.github.com>
2021-03-03 20:50:55 -0800

    database change

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "104.196.229.127",
-  USER: "appuser",
-  PASSWORD: "apppwd",
+  HOST: "139.59.73.251",
+  USER: "dbuser",
+  PASSWORD: "dbVplusU@21",
   DB: "vplusu"
 };
